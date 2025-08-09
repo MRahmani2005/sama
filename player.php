13fe2d48de9743e43d4785bf0022f6b00f6d2d45
unknown <mo.rahmani1384@gmail.com>
2025-08-09 11:21:30 +0330

    version haro tagheer dadam

--- player.php
+++ player.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
   <!-- فایل‌های css و js با جلوگیری از کش -->
-  <link rel="stylesheet" href="style.css?v=20250822">
+  <link rel="stylesheet" href="style.css?v=20250823">
 </head>
 
 <body>
